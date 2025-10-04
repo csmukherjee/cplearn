9bf4d01c587d1834d01d377861a1366075b16375
Spectral things <chandrasekhar.mukherjee07@gmail.com>
2025-10-04 00:52:42 -0700

    New version-ranking finished for now.

--- cplearn_v3/corespect/layer_extraction.py
+++ cplearn_v3/corespect/layer_extraction.py
@@ -11,10 +11,6 @@ import leidenalg
 import networkx as nx
 
 from ..utils.gen_utils import get_kNN
-
-
-
-
 from ..utils.clustering_algo import louvain
 
 
@@ -38,8 +34,6 @@ def cluster_subset(G,core_nodes,res):
     labels = -1 * np.ones(n, dtype=int)
     labels[id_map] = partition.membership
     return labels
-
-
 
 
 def partitioned_majority_addition(edge_list,deg_list,n, cnum, final_labels, candidate_set_mask, eps=0):
@@ -82,9 +76,11 @@ def partitioned_max_addition(edge_list,deg_list,n, cnum, final_labels, candidate
     norm_votes=votes/deg_list.reshape(-1, 1)
 
     max_list=np.max(norm_votes,axis=1)
-    th=max(np.quantile(max_list, 0.75),0.1)
+    nonzero_vals = max_list[max_list != 0]
+    th = np.quantile(nonzero_vals, 0.75)
+#    th=np.quantile(max_list, 0.75)
 
-    print([np.quantile(max_list, 0.1*ell) for ell in range(10)],flush=True)
+    #print([np.quantile(max_list, 0.1*ell) for ell in range(10)],flush=True)
 
 
     for u in range(n):
@@ -139,6 +135,7 @@ def partitioned_majority_stage1(G, cnum, proxy_labels,remaining_nodes, eps):
 
 
 
+
 #Control the second layer here.
 def partitioned_majority_stage2(G, cnum, proxy_labels, remaining_nodes):
     round_num=0
@@ -151,15 +148,17 @@ def partitioned_majority_stage2(G, cnum, proxy_labels, remaining_nodes):
     candidate_set_mask=np.zeros(n).astype(int)
     candidate_set_mask[remaining_nodes]=1
 
+    layer_list=[]
+
     while True:
         n0 = np.sum(proxy_labels != -1)
 
-        proxy_labels, remaining_nodes, added_nodes = partitioned_max_addition(edge_list,deg_list,n, cnum, proxy_labels, candidate_set_mask, eps=0)
+        proxy_labels, candidate_set_mask, added_nodes = partitioned_max_addition(edge_list,deg_list,n, cnum, proxy_labels, candidate_set_mask, eps=0)
 
         for ell in added_nodes:
             layer.append(ell)
 
-
+        layer_list.append(added_nodes)
 
         n1 = np.sum(proxy_labels != -1)
 
@@ -167,14 +166,14 @@ def partitioned_majority_stage2(G, cnum, proxy_labels, remaining_nodes):
         if n1 - n0 < tolerance:
             remaining_nodes=candidate_set_mask.nonzero()[0]
 
-            return layer,proxy_labels,remaining_nodes
+            return layer,proxy_labels,remaining_nodes,layer_list
 
 
 
         if n0 > 0.95 * G.vcount():
             remaining_nodes =candidate_set_mask.nonzero()[0]
 
-            return layer,proxy_labels,remaining_nodes
+            return layer,proxy_labels,remaining_nodes,layer_list
 
         round_num += 1
 
@@ -218,11 +217,74 @@ def find_two_layers_intersection(G,core_nodes,rem_nodes,eps,res):
 
     #Remaining nodes need to be updated to be reflected here.
 
-    layer2, proxy_labels_init, remaining_nodes_init = partitioned_majority_stage2(G, cnum, proxy_labels_init,
+    layer2, proxy_labels_init, remaining_nodes_init,layer_list = partitioned_majority_stage2(G, cnum, proxy_labels_init,
                                                                                   remaining_nodes_init)
-    layer = layer1 + layer2
+ #   layer = layer1 + layer2
+ #   return layer
+    f_layer_list=[layer1]
+    f_layer_list.extend(layer_list)
 
-    return layer
+    return f_layer_list
+
+
+def merge_until_threshold(list_set, n,size_th_frac=0.1):
+    threshold = int(size_th_frac * n)
+    merged, cur = [], []
+    cur_len = 0
+
+    for lst in list_set:
+        cur.extend(lst)
+        cur_len += len(lst)
+        if cur_len >= threshold:
+            merged.append(cur)
+            cur, cur_len = [], 0
+
+    if cur:
+        merged.append(cur)
+
+    # Ensure last block also meets threshold (if possible)
+    if len(merged) > 1 and len(merged[-1]) < threshold:
+        merged[-2].extend(merged[-1])
+        merged.pop()
+
+    return merged
+
+
+#Here we use the list of lists to obtain a cleaner list. This is a fast process.
+def process_list_of_layers(list_of_layer_list,n):
+
+    Layer_list=[]
+    iter_num=len(list_of_layer_list)
+
+    cur_layer=[[] for _ in range(iter_num)]
+    t=0
+    c=0
+    while True:
+
+        for ell in range(0,iter_num):
+            if t<len(list_of_layer_list[ell]):
+                cur_layer[ell].extend(list_of_layer_list[ell][t])
+
+            else:
+                c+=1
+
+        sets = [set(lst) for lst in cur_layer]
+
+        # Intersect all
+        common = set.intersection(*sets)
+        Layer_list.append(list(common))
+
+        for ell in range(iter_num):
+            cur_layer[ell]=[x for x in cur_layer[ell] if x not in common]
+
+        t+=1
+        if c>=iter_num:
+
+            Layer_list=merge_until_threshold(Layer_list,n)
+
+            return Layer_list
+
+
 
 
 
@@ -231,23 +293,36 @@ def find_intersection_layers(G, core_layer,rem_nodes=None,eps=0,mode='one_layer'
 
     if mode == 'one_layer':
         layer_list = Parallel(n_jobs=10)(delayed(find_one_layer_intersection)(G,core_layer,rem_nodes,eps,res) for _ in range(10))
+        print([len(layer) for layer in layer_list])
 
+        layer_candidate=set(layer_list[0])
+        for i in range(1,10):
+            layer_candidate = layer_candidate.intersection(set(layer_list[i]))
+
+        return layer_candidate
 
     elif mode == 'two_layers':
-        layer_list = Parallel(n_jobs=10)(delayed(find_two_layers_intersection)(G,core_layer,rem_nodes,eps,res) for _ in range(10))
+        list_of_layer_list = Parallel(n_jobs=10)(delayed(find_two_layers_intersection)(G,core_layer,rem_nodes,eps,res) for _ in range(10))
+
+        print("Started processing")
+
+        Layer_list=process_list_of_layers(list_of_layer_list,G.vcount())
+
+        print("Finished processing")
+
+        return Layer_list
+
 
     else:
         raise KeyError(f"{mode} is not a valid mode for finding intersection layers")
 
 
-    print([len(layer) for layer in layer_list])
-
-    layer_candidate=set(layer_list[0])
-    for i in range(1,10):
-        layer_candidate = layer_candidate.intersection(set(layer_list[i]))
 
 
-    return layer_candidate
+
+
+
+
 
 
 
@@ -305,14 +380,24 @@ def stable_rank(self,layer_extraction_params=None):
         for node in layer:
             curr_nodes.append(node)
 
-    #Get second stable layer. #This needs fixing.
-    layer_candidate = find_intersection_layers(G, np.array(curr_nodes).astype(int),eps=eps,mode='two_layers',res=res)
-    Layers.append(list(layer_candidate))
+    #Get second stable layer. #This needs fixing. #Here let's get it layer-by-layer?
+    #Probably remove stability?
 
-    print("\n Second stable layer size=",len(layer_candidate),'\n')
+    print("Remaining layers:")
+
+    New_set_of_layers = find_intersection_layers(G, np.array(curr_nodes).astype(int),eps=eps,mode='two_layers',res=res)
+    for layer in New_set_of_layers:
+        Layers.append(layer)
+        remaining_nodes = remaining_nodes - (set(layer))
+        print(len(layer),end=' ')
+
+
+#    Layers.append(list(layer_candidate))
+
+#    print("\n Second stable layer size=",len(layer_candidate),'\n')
 
     #The remaining is the last layer.
-    remaining_nodes=remaining_nodes-(set(layer_candidate))
+#    remaining_nodes=remaining_nodes-(set(layer_candidate))
     layer=[]
     for i in remaining_nodes:
         layer.append(i)
--- data_call.py
+++ data_call.py
@@ -1,4 +1,4 @@
-from cProfile import label
+
 
 import numpy as np
 import pandas as pd
